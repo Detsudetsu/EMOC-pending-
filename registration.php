f20c0872f4eb3b0361cc570364a2abefe4b9de26
kawashima <revirkw36@gmail.com>
2019-01-13 13:51:01 +0900

    新規会員登録システム作成

---
+++ registration.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>registration form</title>
+    <link rel="stylesheet" href="main.css">
+  </head>
+  <body>
+      <h2>新規ユーザー登録</h2>
+      <form action="user_submit.php" method="get">
+      ユーザ名<br><input type="text" name="username"><br>
+      パスワード<br><input type="password" name="password"><br>
+      <input type="submit" value="登録">
+      </form>
+  </body>
+</html>
