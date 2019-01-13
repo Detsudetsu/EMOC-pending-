2a1948d677a13a2d80e84cad606c8f856ba5c600
kawashima <revirkw36@gmail.com>
2019-01-14 03:35:41 +0900

    表記の修正

--- logout.php
+++ logout.php
@@ -8,11 +8,11 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Login success</title>
-    <link rel="stylesheet" href="myblog_style.css">
+    <title>Logout</title>
+    <link rel="stylesheet" href="main.css">
   </head>
   <body>
       <h2>ログアウトしました</h2>
-      <p><a href="top.php">ブログのページに戻る</a></p>
+      <p><a href="top.php">ログイン画面にもどる</a></p>
   </body>
 </html>
